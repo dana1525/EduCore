9668bd4beb99432948ef6e09d824e4ad2cdcc795
dana1525 <mirescudany@gmail.com>
2026-05-18 14:07:44 +0300

    executeInsert added

--- src/main/java/com/elearning/repository/GenericRepository.java
+++ src/main/java/com/elearning/repository/GenericRepository.java
@@ -13,7 +13,7 @@ public abstract class GenericRepository<T> {
     public abstract void update(T entity) throws SQLException;
     public abstract void delete(int id) throws SQLException;
 
-    // helper function for all repositories
+    // helper functions for all repositories
     protected void executeUpdate(String sql, Object... params){
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             for (int i = 0; i < params.length; i++) {
@@ -22,6 +22,21 @@ public abstract class GenericRepository<T> {
             stmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Database error: " + e.getMessage(), e);
+        }
+    }
+
+    protected int executeInsert(String sql, Object... params) throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) { // pentru a recupera id-ul
+            for (int i = 0; i < params.length; i++) {
+                stmt.setObject(i, params[i]);
+            }
+            stmt.executeUpdate();
+
+            ResultSet keys = stmt.getGeneratedKeys();
+            if (keys.next()) {
+                return keys.getInt(1);
+            }
+            throw new RuntimeException("Failed to retrieve generated ID.");
         }
     }
 }
--- src/main/java/com/elearning/repository/StudentRepository.java
+++ src/main/java/com/elearning/repository/StudentRepository.java
@@ -6,34 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentRepository extends GenericRepository<Student> {
+    private Student mapRow(ResultSet rs) throws SQLException {
+        return new Student(
+                rs.getInt("id"),
+                rs.getString("name"),
+                rs.getString("email"),
+                rs.getString("password"),
+                rs.getDouble("progress")
+        );
+    };
 
     // CREATE
     public Student save(Student student) throws SQLException {
         String sqlUser = "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)";
         String sqlStudent = "INSERT INTO students (user_id, progress) VALUES (?, ?)";
+        connection.setAutoCommit(false);
         try {
-            connection.setAutoCommit(false);
-            int generatedId;
-
-            try (PreparedStatement stmt = connection.prepareStatement(sqlUser, Statement.RETURN_GENERATED_KEYS)) { // pentru a recupera id-ul
-                stmt.setString(1, student.getName());
-                stmt.setString(2, student.getEmail());
-                stmt.setString(3, student.getPassword());
-                stmt.setString(4, student.getRole());
-                stmt.executeUpdate();
-
-                ResultSet keys = stmt.getGeneratedKeys();
-                if (keys.next()) {
-                    generatedId = keys.getInt(1);
-                } else {
-                    throw new RuntimeException("Failed to retrieve generated ID.");
-                }
-            }
-            try(PreparedStatement stmt = connection.prepareStatement(sqlStudent)) {
-                stmt.setInt(1, generatedId);
-                stmt.setDouble(2, student.getProgress());
-                stmt.executeUpdate();
-            }
+            int generatedId = executeInsert(sqlUser, student.getName(),
+                    student.getEmail(), student.getPassword(), student.getRole());
+            executeInsert(sqlStudent, generatedId, student.getProgress());
             connection.commit();
             student.setId(generatedId);
             return student;
@@ -51,10 +42,8 @@ public class StudentRepository extends GenericRepository<Student> {
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
-
             if (rs.next()) {
-                return new Student(rs.getInt("id"), rs.getString("name"),
-                        rs.getString("email"), rs.getString("password"), rs.getDouble("progress"));
+                return mapRow(rs);
             }
         }
         return null;
@@ -65,10 +54,8 @@ public class StudentRepository extends GenericRepository<Student> {
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setString(1, email);
             ResultSet rs = stmt.executeQuery();
-
             if (rs.next()) {
-                return new Student(rs.getInt("id"), rs.getString("name"),
-                        rs.getString("email"), rs.getString("password"), rs.getDouble("progress"));
+                return mapRow(rs);
             }
         }
         return null;
@@ -79,17 +66,14 @@ public class StudentRepository extends GenericRepository<Student> {
         String sql = "SELECT * FROM users join students on users.id = students.user_id";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             ResultSet rs = stmt.executeQuery();
-
             while (rs.next()) {
-                Student student = new Student(rs.getInt("id"), rs.getString("name"),
-                        rs.getString("email"), rs.getString("password"), rs.getDouble("progress"));
-                students.add(student);
+                students.add(mapRow(rs));
             }
         }
         return students;
     }
 
-    // UPDATE
+    // UPDATE (transaction in this method)
     public void update(Student student) throws SQLException {
         String sqlUser = "UPDATE users SET name = ?, email = ?, password = ? WHERE users.id = ?";
         String sqlStudent = "UPDATE students SET progress = ? WHERE user_id = ?";
